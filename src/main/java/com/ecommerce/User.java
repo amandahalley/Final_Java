6d1abfc37a2766686bcbd3095fef655b9ebffe53
ahllyy <amanda.halley@keyin.com>
2024-12-08 18:06:05 -0330

    created all required classes to be completed

---
+++ src/main/java/com/ecommerce/User.java
@@ -0,0 +1,60 @@
+package com.ecommerce;
+
+public abstract class User {
+    private int id;
+    private String username;
+    private String email;
+    private String role;
+
+    //Constructor
+    public User(int id, String username, String email, String role) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.role = role;
+    }
+
+    //Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    //Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
