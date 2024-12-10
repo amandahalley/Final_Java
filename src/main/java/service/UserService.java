5ce0ca19db6f16d932783b424c990b718030590c
ahllyy <amanda.halley@keyin.com>
2024-12-10 19:56:36 -0330

    added service logic for user registration and login with password hashing

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,4 +1,36 @@
 package service;
 
+import DAO.UserDAO;
+import com.ecommerce.User;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.SQLException;
+
 public class UserService {
-}
+    private final UserDAO userDAO;
+
+    public UserService(UserDAO userDAO) {
+        this.userDAO = userDAO;
+
+    }
+    //Register user with encrypted password
+    public void registerUser(String username, String email, String password, String role) throws SQLException {
+        //hash password before storing it
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+        userDAO.registerUser(username, email, hashedPassword, role);
+    }
+
+    //Login by verifying password
+    public User login(String username, String password) throws SQLException {
+        //Fetch user from database
+        User user = userDAO.getUserByUsername(username);
+        if (user != null) {
+            String storedPassword = userDAO.getPasswordByUsername(username);
+            //Verify
+            if (BCrypt.checkpw(password, storedPassword)) {
+                return user;
+            }
+        }
+        return null;
+    }
+    }
